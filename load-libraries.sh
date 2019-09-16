b62d2ec814136f1859762ad5a5a11affb911b6da
CDEP CREATED ACCOUNT <test@cloudera.com>
2019-09-16 00:10:54 +0000

    Tensorboard Start Script Added

--- load-libraries.sh
+++ load-libraries.sh
@@ -6,6 +6,7 @@ os.chdir('/home/cdsw/demo/models')
 !pip2 install keras
 !pip2 install sklearn
 !pip install scipy --upgrade
+!pip2 install wait
 
 #os.system('nohup /home/cdsw/.local/bin/tensorboard --logdir /home/cdsw/demo/tensorboard/tf_files/training_summaries --port  8090 --host localhost &')
 
